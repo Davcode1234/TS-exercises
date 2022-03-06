aa89e867b062c3aec8e00eab7598460c36101c43
Davcode1234 <dpietruszewski12@gmail.com>
2022-03-06 14:42:28 +0100

    Add functionality for push tasks into array

--- dist/app.js
+++ dist/app.js
@@ -2,4 +2,24 @@
 //   <label for="task-1">Wyrzucić śmieci</label>
 //   <input type="checkbox" id="task-1" name="Wyrzucić śmieci"/>
 // </li>
+const taskContainerElement = document.querySelector(".tasks");
+const taskNameInputElement = document.querySelector("#name");
+const addButtonElement = document.querySelector("button");
 const tasks = ["Wyrzucić śmieci", "Pójść na siłkę", "Nakarmić koty"];
+const render = () => {
+    taskContainerElement.innerHTML = "";
+    tasks.forEach(task => {
+        const taskElement = document.createElement("li");
+        taskElement.innerText = task;
+        taskContainerElement.appendChild(taskElement);
+    });
+};
+const addTask = (task) => {
+    tasks.push(task);
+};
+addButtonElement.addEventListener("click", (event) => {
+    event.preventDefault();
+    addTask(taskNameInputElement.value);
+    render();
+});
+render();
--- src/app.ts
+++ src/app.ts
@@ -3,4 +3,29 @@
 //   <input type="checkbox" id="task-1" name="Wyrzucić śmieci"/>
 // </li>
 
+const taskContainerElement: HTMLElement = document.querySelector(".tasks")
+const taskNameInputElement: HTMLInputElement = document.querySelector("#name")
+const addButtonElement: HTMLButtonElement = document.querySelector("button")
+
 const tasks: string[] = ["Wyrzucić śmieci", "Pójść na siłkę", "Nakarmić koty"];
+
+const render = () => {
+  taskContainerElement.innerHTML = "";
+  tasks.forEach(task => {
+    const taskElement: HTMLElement = document.createElement("li")
+    taskElement.innerText = task;
+    taskContainerElement.appendChild(taskElement)
+  })
+}
+
+const addTask = (task: string) => {
+  tasks.push(task)
+}
+
+addButtonElement.addEventListener("click", (event: Event) => {
+  event.preventDefault();
+  addTask(taskNameInputElement.value)
+  render();
+})
+
+render();
